673dc2117b7e4ba43325cd669b1fa84c11fbf2d5
Abai <nogoibaev_a@auca.kg>
2021-12-13 18:48:24 +0600

    Final Exam by Nogoibaev Abai

---
+++ app/src/main/java/com/example/nogoibaev_abai_final/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nogoibaev_abai_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.yourorder)
+    }
+}
